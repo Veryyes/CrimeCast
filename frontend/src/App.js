c69f03e8b8fc822f07eba9279052aaaf9860fede
Brandon <veryyes10@yahoo.com>
2018-03-24 17:22:02 -0400

    Maps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {  withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import './App.css';
 import Modal from './Modal.js';
 
@@ -69,8 +70,20 @@ class App extends Component {
 
 class Map extends Component{
 	render(){
+	const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+		  <GoogleMap
+   			 defaultZoom={8}
+   			 defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  			>
+    		{props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  			</GoogleMap>
+		))
 		return(
-			<div className="Map">
+			<div className="Map">  
+    			<MyMapComponent isMarkerShown   googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}/>	
 			</div>
 		);
 	}
