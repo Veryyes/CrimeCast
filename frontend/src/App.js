a2ef8645af8ea173ca7a917cad31fcc84d731af4
Brandon <veryyes10@yahoo.com>
2018-03-24 22:58:41 -0400

    Moved map to its own file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import {  withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-import {MarkerWithLabel} from "react-google-maps/lib/components/addons/MarkerWithLabel"
 import './App.css';
 import Modal from './Modal.js';
+import Map from './Map.js';
+
+/*
+var mysql = require('mysql');
+console.log(mysql);
+var con = mysql.createConnection({
+	host: "35.231.114.23",
+	user: "crimecast",
+	password: "badboi",
+	database: "crime"
+});
+*/
+
+/*var unirest = require('unirest');
+var spotcrime = require('spotcrime');*/
 
 class App extends Component {
 	constructor(props){
@@ -68,83 +81,6 @@ class App extends Component {
 }
 
 
-
-class Map extends Component<GeolocatedProps>{
-	constructor(props){
-		super(props);
-	
-		this.state = {
-		
-			gps: {
-				lat: 38.0316816,
-				lng: -78.5135989
-			},
-			markers: [
-				{lat:38,lng:-78, descript:"Property Damage"},
-				{lat:37,lng:-77, descript:"Robbery"},
-			]
-		}
-		this.success = this.success.bind(this);
-	}
-//	{props.isMarkerShown && <Marker position={{ lat: 38.0316816, lng: -78.5135989 }} />}
-
-	success(pos){
- 	 let crd = pos.coords;
-	
-	 this.setState((prevState) =>{
-		return{
-			gps:{
-				lat: crd.latitude,
-				lng: crd.longitude
-			}
-		}
-	});
-	console.log(crd);	
-	}
-	error(err) {
-	  console.warn(`ERROR(${err.code}): ${err.message}`);
-	}
-
-	render(){
-		let options = {
- 			enableHighAccuracy: true,
- 			timeout: 5000,
- 			maximumAge: 0
-		};
-		navigator.geolocation.getCurrentPosition(this.success,this.error, options);
-		let MyMapComponent = withScriptjs(withGoogleMap((props) =>
-			 <GoogleMap
-   				 defaultZoom={8}
-   				 defaultCenter={{ lat: this.state.gps.lat, lng: this.state.gps.lng }}>
-				{props.isMarkerShown && <Marker position={{ lat: this.state.gps.lat, lng: this.state.gps.lng  }} />}
-				{props.markers.map(marker=>(
-					<MarkerWithLabel
-						position={{lat: marker.lat, lng: marker.lng}}
-						labelAnchor={(0,0)}
-						labelStyle={{backgroundColor: "#000000", color:"#ffffff",fontSize: "12px", padding:"2px"}}
-					>
-						<div>
-							{marker.descript}
-						</div>
-					</MarkerWithLabel>
-				))}
-  			</GoogleMap>
-		));
-		return(
-			<div className="Map">  
-			
-    		<MyMapComponent 
-				isMarkerShown 
-				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQAsVsOK3RIivTBZpb0JpED65vUUZIAzo&v=3.exp&libraries=geometry,drawing,places"
- 				loadingElement={<div style={{ height: `100%` }} />}
-  				containerElement={<div style={{ height: `400px` }} />}
-  				mapElement={<div style={{ height: `100%` }} />}
-				markers={this.state.markers}
-			/>	
-			</div>
-		);
-	}
-}
 
 class Header extends Component{
 	render(){
